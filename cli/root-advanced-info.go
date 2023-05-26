5cbad8183461672551a5f7e1cd547d47ed2ccffc
Shashank S. Harivyasi <45761248+harivyasi@users.noreply.github.com>
2023-05-26 13:27:55 +0200

    suggested changes

--- cli/root-advanced-info.go
+++ cli/root-advanced-info.go
@@ -2,11 +2,37 @@ package cli
 
 import (
 	"fmt"
+	"math"
 	"runtime"
 	"time"
 
+	"github.com/chigopher/pathlib"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
 	"github.com/spf13/cobra"
 )
+
+func getDiskSpace() (msg string) {
+	if diskStat, err := disk.Usage(pathlib.NewPath("/").String()); err == nil {
+		total := float64(diskStat.Total) / math.Pow(2, 30) // Convert from bytes to GiB
+		free := float64(diskStat.Free) / math.Pow(2, 30)   // Convert from bytes to GiB
+		msg = fmt.Sprintf("\n- Disk space:\n  - %7.1fGi (total) %7.1fGi (free)", total, free)
+	} else {
+		zboth.Warn().Err(err).Msgf("Failed to retrieve information about disk space.")
+	}
+	return
+}
+
+func getMemory() (msg string) {
+	if memStat, err := mem.VirtualMemory(); err == nil {
+		total := float64(memStat.Total) / math.Pow(2, 30)    // Convert from bytes to GiB
+		free := float64(memStat.Available) / math.Pow(2, 30) // Convert from bytes to GiB
+		msg = fmt.Sprintf("\n- Memory:\n  - %7.1fGi (total) %7.1fGi (free)", total, free)
+	} else {
+		zboth.Warn().Err(err).Msgf("Failed to retrieve information about memory.")
+	}
+	return
+}
 
 // get system information
 func getSystemInfo() (info string) {
