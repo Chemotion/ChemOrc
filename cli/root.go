8ec229976fb8c1d57d74a7d99491ee534500571a
PiTrem <pierre.tremouilhac@kit.edu>
2024-12-18 11:13:21 +0100

    chore:  v2.19

--- cli/root.go
+++ cli/root.go
@@ -68,7 +68,7 @@ const (
 // configuration and logging
 var (
 	// version number, here to allow override
-	versionCLI = "0.2.18"
+	versionCLI = "0.2.19"
 	// current shell
 	shell string
 	// currently selected instance
