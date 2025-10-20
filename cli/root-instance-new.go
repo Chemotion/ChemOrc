2f46558eb00b4955dd2d315452736de50983e963
Shashank S. Harivyasi <harivyasi@gmail.com>
2025-10-20 14:24:36 +0200

    update code to point to point to version 2.1.1 of ELN - also README

--- cli/root-instance-new.go
+++ cli/root-instance-new.go
@@ -15,9 +15,9 @@ import (
 // helper to determine the required compose file
 func getComposeAddressToUse(currentVersion, action string) (use string) {
 	versions := make(map[string]string)
-	latestForThisCLIRelease := "2.0.1"
+	latestForThisCLIRelease := "2.1.1"
 	orderVersions := []string{latestForThisCLIRelease, "1.10.5", "1.9.3"} // descending order
-	versions[latestForThisCLIRelease] = "https://raw.githubusercontent.com/Chemotion/ChemOrc/47507b7348b6ab6850036b2e334f586aba03fea3/payload/docker-compose.yml"
+	versions[latestForThisCLIRelease] = "https://raw.githubusercontent.com/Chemotion/ChemOrc/f20e4f590ec788613281b2768d45a40fa2c6fa1f/payload/docker-compose.yml"
 	versions["1.10.5"] = "https://raw.githubusercontent.com/Chemotion/ChemOrc/3a9339fe7156da32d786975482aa97c993a997b9/payload/docker-compose.yml"
 	versions["1.9.3"] = "https://raw.githubusercontent.com/Chemotion/ChemCLI/b7ad83fba1e1db6c5525a11b06bf7eed59a769f6/payload/docker-compose.yml"
 	validVersions := []string{}
